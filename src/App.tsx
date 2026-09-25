c5917e3294ebaf127ce93dcacc3ed1ef93b7fda9
Fadil adi maulana <wilzuren@gmail.com>
2026-09-25 16:17:28 +0700

    fix: struktur folder yang benar

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <>
+    <Navbar />
+    <section>Hero (Tentang saya)</section>
+    <section>Yang saya kerjakan</section>
+    <section>Project saya</section>
+    <footer>Kontak</footer>
+    </>
+
+  )
+}
+
+export default App 
