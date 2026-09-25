9847979369d92999e0d3148a2e97fdb2198e52b8
Fadil adi maulana <wilzuren@gmail.com>
2026-09-25 17:59:47 +0700

    feat: isi konten About, Skills, Project, Contact

---
+++ src/components/Contact.tsx
@@ -0,0 +1,36 @@
+function Contact() {
+    return (
+        <footer id="contact" className="px-6 py-16 text-center border-t">
+            <h2 className="text-2xl font-bold mb-6">Kontak</h2>
+            <div className="flex flex-col items-center gap-3">
+
+                <a href="https://wa.me/6282117689752"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-600 underline"
+                >
+                    WhatsApp
+                </a>
+
+                <a href="https://instagram.com/dilmlnaa"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-600 underline"
+                >
+                    Instagram
+                </a>
+
+                <a href="https://github.com/Fadil2112N"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-600 underline"
+                >
+                    GitHub
+                </a>
+            </div>
+            
+        </footer>
+    )
+}
+
+export default Contact 
